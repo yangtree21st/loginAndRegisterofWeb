f002a7942404256f559276647d831fbe60447057
yang shu <yangtree21st@gmail.com>
2019-05-02 18:39:22 -0400

    initial commit

---
+++ src/main/java/org/zipcode/registAndLogin/RegistAndLoginApplication.java
@@ -0,0 +1,13 @@
+package org.zipcode.registAndLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistAndLoginApplication.class, args);
+	}
+
+}
